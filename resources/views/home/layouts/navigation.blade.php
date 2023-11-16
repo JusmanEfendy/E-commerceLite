54142edaa42762c6acbc61ed543fb29b0c564b41
JusmanEfendy <njhussrhiee0206@gmail.com>
2023-11-16 21:51:09 +0700

    add Sweetalert

--- resources/views/home/layouts/navigation.blade.php
+++ resources/views/home/layouts/navigation.blade.php
@@ -13,6 +13,11 @@
     <link href="{{ asset('bootstrap5/css/bootstrap.min.css') }}" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="{{ asset('home/css/styles.css') }}" rel="stylesheet" />
+
+    <!-- jQuery -->
+    <script src="{{ asset('template') }}/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('template') }}/plugins/jquery-ui/jquery-ui.min.js"></script>
 </head>
 
 <body>
@@ -43,10 +48,22 @@
                 </ul>
                 <form action="{{ route('logout') }}" method="post" class="d-flex gap-2">
                     @csrf
+                    @php
+                        $pesanan = \App\Models\Pesanan::where('user_id', optional(Auth::user())->id)
+                            ->where('status', 0)
+                            ->first();
+
+                        if ($pesanan && $pesanan->exists()) {
+                            $notif = \App\Models\PesananDetail::where('pesan_id', $pesanan->id)->count();
+                        }
+                    @endphp
+
+
                     <a href="{{ url('/shop/chart') }}" class="btn btn-outline-dark" type="submit">
                         <i class="bi-cart-fill me-1"></i>
                         Cart
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        <span class="badge bg-dark text-white ms-1 rounded-pill">{{ isset($notif) ? $notif : 0 }}
+                        </span>
                     </a>
                     <ul class="navbar-nav ms-auto">
                         @auth
@@ -57,15 +74,18 @@
                                 </a>
                                 <ul class="dropdown-menu">
                                     @if (Auth::user()->role === 'admin')
-                                    <li><a class="dropdown-item" href="{{ route('dashboard') }}"><i class="bi bi-speedometer"></i> Dashboard</a></li>
+                                        <li><a class="dropdown-item" href="{{ route('dashboard') }}"><i
+                                                    class="bi bi-speedometer"></i> Dashboard</a></li>
                                     @endif
-                                    <li><a class="dropdown-item" href="{{ route('profile.edit') }}"><i class="bi bi-person"></i> Profile</a></li>
+                                    <li><a class="dropdown-item" href="{{ route('profile.edit') }}"><i
+                                                class="bi bi-person"></i> Profile</a></li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
                                     <li><a class="dropdown-item" href="LogOut"
-                                        onclick="event.preventDefault();
-                                                    this.closest('form').submit();"><i class="bi bi-box-arrow-right"></i> Logout
+                                            onclick="event.preventDefault();
+                                                    this.closest('form').submit();"><i
+                                                class="bi bi-box-arrow-right"></i> Logout
                                         </a></li>
                                 </ul>
                             </li>
