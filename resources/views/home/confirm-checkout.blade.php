ec08531135500a19e118bac9b8d4c26ca2e92fb7
JusmanEfendy <njhussrhiee0206@gmail.com>
2023-11-19 00:40:19 +0700

    update home confir checkout

--- resources/views/home/confirm-checkout.blade.php
+++ resources/views/home/confirm-checkout.blade.php
@@ -1,23 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Konfirmasi Pembayaran</title>
-    <link rel="stylesheet" href="{{ asset('bootstrap5/css/bootstrap.min.css') }}">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <a href="" class="btn btn-danger">kembali</a>
-            </div>
-        </div>
-    </div>
-    <script src="{{ asset('bootstrap5/js/bootstrap.bundle.min.js') }}"></script>
-</body>
-</html> --}}
 
 <title>{{ $title }}</title>
 
