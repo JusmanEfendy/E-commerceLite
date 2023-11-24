9e7ee460cd780cf32ac6a4fce3ec35b9afc2bbb6
JusmanEfendy <njhussrhiee0206@gmail.com>
2023-11-24 21:55:37 +0700

    add validasi in page keranjang

--- resources/views/home/keranjang.blade.php
+++ resources/views/home/keranjang.blade.php
@@ -110,7 +110,7 @@
                         
                         <div class="row">
                             <div class="col-md-12">
-                                <button type="submit" {{ !empty($pesanan) ? '' : 'disabled' }} class="btn btn-outline-dark btn-lg py-3 mb-2">Checkout</button>
+                                <button type="submit" {{ $pesanan->total_harga === 0 ? 'disabled' : '' }} {{ !empty($pesanan) ? '' : 'disabled' }} class="btn btn-outline-dark btn-lg py-3 mb-2">Checkout</button>
                             </div>
                         </div>
                     </form>
